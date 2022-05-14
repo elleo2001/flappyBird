b7431151a7e14d051bf35e5e65365ddbafb3a9e7
elleo2001 <93232011+elleo2001@users.noreply.github.com>
2022-05-14 17:53:21 -0300

    Flappy

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -2,7 +2,7 @@ function novoElemento(tagName, className) {
     const elem = document.createElement(tagName)
     elem.className = className
     return elem
-}
+} 
  
 function Barreira(reversa = false) {
     this.elemento = novoElemento("div", "barreira")
